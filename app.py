dc32a806ece7133dbc5210f2f1e55f91136ba35a
Meenakshi Vashistha <71630955+VashisthaMeenakshi15@users.noreply.github.com>
2026-02-08 00:08:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ st.markdown("""
 
     /* Sidebar Background */
     [data-testid="stSidebar"] {
-        background-color: #13293D; /* Navy */
+        background-color: #13293D; /* Sky Blue */
     }
     
     /* Sidebar Text Color */
